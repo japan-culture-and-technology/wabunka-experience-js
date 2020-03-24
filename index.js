5fcad4c06fb04eaa700bda7c2088155f97bf746f
35d <xxx35d@gmail.com>
2020-03-24 13:43:14 +0900

    ✨ first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+jQuery(function() {
+  var isEnglishPage = location.href.includes("en.");
+  if (isEnglishPage) {
+    var $rotateTargetBlock = jQuery("#js-block03-0 .p-block03__header-title");
+    // console.log($rotateTargetBlock);
+    $rotateTargetBlock.css("writing-mode", "horizontal-tb");
+    $rotateTargetBlock.text("Tsukiji Honganji");
+  }
+});
