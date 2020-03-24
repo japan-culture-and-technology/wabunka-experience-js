af0acc9c6ab0da78b6818942505274f945de2864
35d <xxx35d@gmail.com>
2020-03-24 13:45:18 +0900

    🐛 comment out

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-jQuery(function() {
-  var isEnglishPage = location.href.includes("en.");
-  if (isEnglishPage) {
-    var $rotateTargetBlock = jQuery("#js-block03-0 .p-block03__header-title");
-    // console.log($rotateTargetBlock);
-    alert('@@');
-    $rotateTargetBlock.css("writing-mode", "horizontal-tb");
-    $rotateTargetBlock.text("Tsukiji Honganji");
-  }
-});
+// jQuery(function() {
+//   var isEnglishPage = location.href.includes("en.");
+//   if (isEnglishPage) {
+//     var $rotateTargetBlock = jQuery("#js-block03-0 .p-block03__header-title");
+//     // console.log($rotateTargetBlock);
+//     alert('@@');
+//     $rotateTargetBlock.css("writing-mode", "horizontal-tb");
+//     $rotateTargetBlock.text("Tsukiji Honganji");
+//   }
+// });
