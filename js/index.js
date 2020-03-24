17305fc727b5c7a289994afd0cf242b785e14b50
35d <xxx35d@gmail.com>
2020-03-24 14:23:57 +0900

    ✨ fix

--- js/index.js
+++ js/index.js
@@ -1,11 +1,8 @@
-alert('@');
-
 jQuery(function () {
   var isEnglishPage = location.href.includes("en.");
   if (isEnglishPage) {
     var $rotateTargetBlock = jQuery("#js-block03-0 .p-block03__header-title");
     // console.log($rotateTargetBlock);
-    alert('@@');
     $rotateTargetBlock.css("writing-mode", "horizontal-tb");
     $rotateTargetBlock.text("Tsukiji Honganji");
   }
