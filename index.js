2c67e803243acdd4b5b56d3bfdb726f2367e2dc6
35d <xxx35d@gmail.com>
2020-03-24 14:32:55 +0900

    🗑 delete alert

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ jQuery(function () {
   if (isEnglishPage) {
     var $rotateTargetBlock = jQuery("#js-block03-0 .p-block03__header-title");
     // console.log($rotateTargetBlock);
-    alert('@@');
     $rotateTargetBlock.css("writing-mode", "horizontal-tb");
     $rotateTargetBlock.text("Tsukiji Honganji");
 
