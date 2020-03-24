0b8fc8e46c4d01946025ce69568b7b482bb6a98e
35d <xxx35d@gmail.com>
2020-03-24 14:32:24 +0900

    ✨ hide sidebar

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 jQuery(function () {
+
   var isEnglishPage = location.href.includes("en.");
   if (isEnglishPage) {
     var $rotateTargetBlock = jQuery("#js-block03-0 .p-block03__header-title");
@@ -6,5 +7,9 @@ jQuery(function () {
     alert('@@');
     $rotateTargetBlock.css("writing-mode", "horizontal-tb");
     $rotateTargetBlock.text("Tsukiji Honganji");
+
+    // 「書道」「茶道」「華道」ボタンの削除
+    var $sliderNav = jQuery('#js-index-slider__nav');
+    $sliderNav.hide();
   }
 });
