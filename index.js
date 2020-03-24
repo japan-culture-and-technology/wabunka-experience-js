249a4f47e2466ec79cc53c745044c4792b98e751
35d <xxx35d@gmail.com>
2020-03-24 14:53:00 +0900

    ✨ top link

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 jQuery(function () {
-
   var isEnglishPage = location.href.includes("en.");
   if (isEnglishPage) {
     var $rotateTargetBlock = jQuery("#js-block03-0 .p-block03__header-title");
-    // console.log($rotateTargetBlock);
     $rotateTargetBlock.css("writing-mode", "horizontal-tb");
     $rotateTargetBlock.text("Tsukiji Honganji");
 
@@ -11,4 +9,7 @@ jQuery(function () {
     var $sliderNav = jQuery('#js-index-slider__nav');
     $sliderNav.hide();
   }
+
+  // トップページ 開催実績飛ばないようにする
+  jQuery('.p-index-news__list a').attr('href', '#');
 });
