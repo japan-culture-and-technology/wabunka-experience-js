0d06b5727dcc6efb1ec386e67772049e371f569d
35d <xxx35d@gmail.com>
2020-03-24 15:04:03 +0900

    🗑 delete side button

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ jQuery(function () {
     var $rotateTargetBlock = jQuery("#js-block03-0 .p-block03__header-title");
     $rotateTargetBlock.css("writing-mode", "horizontal-tb");
     $rotateTargetBlock.text("Tsukiji Honganji");
-
-    // 「書道」「茶道」「華道」ボタンの削除
-    var $sliderNav = jQuery('#js-index-slider__nav');
-    $sliderNav.hide();
   }
+
+  // 「書道」「茶道」「華道」ボタンの削除
+  var $sliderNav = jQuery('#js-index-slider__nav');
+  $sliderNav.hide();
 
   // トップページ 開催実績飛ばないようにする
   jQuery('.p-index-news__list a').attr('href', '#');
