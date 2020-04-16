4337ce043205a912dd5719e7c53ebf0329f51c26
35d <xxx35d@gmail.com>
2020-04-16 15:34:56 +0900

    ✨ fix nav bar

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ jQuery(function () {
   if (isEnglishPage) {
     // グローバルナビゲーションの出し分け対応
     jQuery('nav a:contains("English")').hide();
-    jQuery('nav a:contains("開催場所例")').hide();
+    jQuery('nav a:contains("開催場所")').hide();
     jQuery('nav a:contains("体験プログラム")').hide();
     jQuery('nav a:contains("メディア")').hide();
     jQuery('nav a:contains("お問い合わせ")').hide();
@@ -16,15 +16,15 @@ jQuery(function () {
   }
 
   // 「書道」「茶道」「華道」ボタンの削除
-  var $sliderNav = jQuery('#js-index-slider__nav');
+  var $sliderNav = jQuery("#js-index-slider__nav");
   $sliderNav.hide();
 
   // トップページ 開催実績飛ばないようにする
-  jQuery('.p-index-news__list a').attr('href', 'javascript:void(0)');
-  
+  jQuery(".p-index-news__list a").attr("href", "javascript:void(0)");
+
   // フッタースライダー削除
-  jQuery('.p-footer-slider').hide();
+  jQuery(".p-footer-slider").hide();
 
   // 記事ページのパンくずリスト削除
-  jQuery('.c-breadcrumb').hide();
+  jQuery(".c-breadcrumb").hide();
 });
