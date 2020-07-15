c4345a7592fbb418a012524258974b04bb4392a7
35d <xxx35d@gmail.com>
2020-07-15 23:15:09 +0900

    ✨ fix

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 jQuery(function () {
-  var isEnglishPage = location.href.includes("en.");
-  if (isEnglishPage) {
-    // グローバルナビゲーションの出し分け対応
-    jQuery('nav a:contains("English")').hide();
-    jQuery('nav a:contains("開催場所")').hide();
-    jQuery('nav a:contains("体験プログラム")').hide();
-    jQuery('nav a:contains("メディア")').hide();
-    jQuery('nav a:contains("お問い合わせ")').hide();
-  } else {
-    jQuery('nav a:contains("日本語")').hide();
-    jQuery('nav a:contains("Place")').hide();
-    jQuery('nav a:contains("Program")').hide();
-    jQuery('nav a:contains("Media")').hide();
-    jQuery('nav a:contains("Contact")').hide();
-  }
+  // var isEnglishPage = location.href.includes("en.");
+  // if (isEnglishPage) {
+  //   // グローバルナビゲーションの出し分け対応
+  //   jQuery('nav a:contains("English")').hide();
+  //   jQuery('nav a:contains("開催場所")').hide();
+  //   jQuery('nav a:contains("体験プログラム")').hide();
+  //   jQuery('nav a:contains("メディア")').hide();
+  //   jQuery('nav a:contains("お問い合わせ")').hide();
+  // } else {
+  //   jQuery('nav a:contains("日本語")').hide();
+  //   jQuery('nav a:contains("Place")').hide();
+  //   jQuery('nav a:contains("Program")').hide();
+  //   jQuery('nav a:contains("Media")').hide();
+  //   jQuery('nav a:contains("Contact")').hide();
+  // }
 
   // トップページ 開催実績飛ばないようにする
   jQuery(".p-index-news__list a").attr("href", "javascript:void(0)");
 
   // フッタースライダー削除
-  jQuery(".p-footer-slider").hide();
+  // jQuery(".p-footer-slider").hide();
 
   // 記事ページのパンくずリスト削除
   // jQuery(".c-breadcrumb").hide();
